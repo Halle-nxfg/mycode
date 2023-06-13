a1579507247485132e03be94a8efc3b20bb69c84
halle-nxfg <halle.gumpel@gmail.com>
2023-06-13 20:31:19 +0000

    terraform null resource loops

--- null-loops/main.tf
+++ null-loops/main.tf
@@ -4,42 +4,40 @@ Working with "for_each" within a null_resource */
 /* Terraform block */
 terraform {
   required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.1.1"
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.22.0"
     }
   }
 }
 
 /* provider block */
-provider "null" {
-  # Configuration options
+provider "docker" {
 }
 
 /* a list of local variables */
 locals {
-  avengers = {"ironman"= "hero"
-              "captain america"= "hero"
-              "thanos"= "villain"
-              "venom"= "anti-hero"
+  avengers = {"ironman"= {extport = 8000, desc = "hero"}
+              "captain_america" = {extport = 8001, desc = "hero"}
+              "thanos" = {extport = 8002, desc = "villain"}
+              "venom" = {extport = 8003, desc = "anti-hero"}
              }
 }
 
+resource "docker_image" "nginx" {   
+    name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
 
-/* The null_resource implements the standard resource lifecycle but takes no more action */
-resource "null_resource" "avengers" {
-  for_each = local.avengers     // local is not a typo, locals.avengers would be incorrect
+resource "docker_container" "avengers" {
+  for_each = local.avengers 
   /* triggers allows specifying a random set of values that when
      changed will cause the resource to be replaced */
-  triggers = {
-    name = each.key  // a special variable, "each" created by terraform
-                       // the object has "each.key" and "each.value"
-    status= each.value
-  }
-}
+    name = each.key  
+    image = docker_image.nginx.image_id
+    ports {
+        internal = 80
+        external = each.value.extport
+    }
 
-/* We want these outputs */
-output "avengers" {
-  value = null_resource.avengers
 }
-
